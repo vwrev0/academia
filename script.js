4166f29e5f414b20f42f7c44d18afc215673ca1a
vwrev0 <145126617+vwrev0@users.noreply.github.com>
2023-09-22 10:49:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function logar(){
+    let usu = document.querySelector("#usu").value;
+    let senha = document.querySelector("#senha").value;
+if (usu === "admin" || senha === 123) {
+    alert("Bem Vindo");
+} else {
+    alert("Perdeu");
+}
+
+}
